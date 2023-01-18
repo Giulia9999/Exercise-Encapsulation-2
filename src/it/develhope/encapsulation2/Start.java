520ea662258dfcbe8bf60c30a84d895784628ec5
Giulia <giuliac35@gmail.com>
2023-01-18 22:31:15 +0100

    Add Scanner class to main method

--- src/it/develhope/encapsulation2/Start.java
+++ src/it/develhope/encapsulation2/Start.java
@@ -1,8 +1,11 @@
 package it.develhope.encapsulation2;
 
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
         Person person = new Person();
 
+        Scanner scanner = new Scanner(System.in);
     }
 }
