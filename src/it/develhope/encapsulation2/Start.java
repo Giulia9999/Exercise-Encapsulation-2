8e9e61b9e4eccb55ec7419628524783f1de6a2f8
Giulia <giuliac35@gmail.com>
2023-01-18 22:42:53 +0100

    Used Scanner methods to print the Person details

--- src/it/develhope/encapsulation2/Start.java
+++ src/it/develhope/encapsulation2/Start.java
@@ -7,5 +7,20 @@ public class Start {
         Person person = new Person();
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert the name of the person: ");
+        person.setName(scanner.nextLine());
+        System.out.printf("Tha name of the person is %s %n", person.getName());
+
+        System.out.print("Insert the surname of the person: ");
+        person.setSurname(scanner.nextLine());
+        System.out.printf("Tha surname of the person is %s %n", person.getSurname());
+
+        System.out.print("Insert the height of the person: ");
+        person.setHeight((scanner.nextDouble()));
+        System.out.printf("Tha height of the person is %s %n", person.getHeight());
+
+        System.out.print("Insert the age of the person: ");
+        person.setAge((scanner.nextInt()));
+        System.out.printf("Tha age of the person is %s %n", person.getAge());
     }
 }
